5210bccf7ed2dd31a919faaf00a99f914516e1c4
Avinash Subhash <avinashsubhash19@gmail.com>
2021-06-01 16:06:19 +0530

    Added some problems of may lunchtime

---
+++ HOOPS.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+#define int1 long long int
+#define endl "\n"
+using namespace std;
+
+void solution()
+{
+    //Solving area
+    int1 num;
+    cin>>num;
+    cout<<(num/2)+1<<endl;
+}
+
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    int1 t;
+    cin>>t;
+    while(t--)
+    {
+        solution();
+    }
+}
