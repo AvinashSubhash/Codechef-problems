87293927eeb2225d15158e7333878be68cef6d7b
AvinashSubhash <avinashsubhash19@gmail.com>
2021-06-07 19:51:50 +0530

    empty-entry

---
+++ codechef-long-june-3.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+#define int1 long long int
+#define endl "\n"
+using namespace std;
+
+void solution()
+{
+    //Solving area
+    cout<<(5&5);
+}
+
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    int1 t;
+    cin>>t;
+    while(t--)
+    {
+        solution();
+    }
+}
