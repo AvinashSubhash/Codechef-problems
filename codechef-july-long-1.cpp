0891e6b67e206f727a318f08b9372e860f32d5fa
AvinashSubhash <avinashsubhash19@gmail.com>
2021-07-09 00:28:18 +0530

    added_changes

--- codechef-july-long-1.cpp
+++ codechef-july-long-1.cpp
@@ -6,54 +6,33 @@ using namespace std;
 void solution()
 {
     //Solving area
-    int1 n,k,ans;
-    cin >>n >> k;
-      vector<int> a(n);
-      for(int& i : a){
-          cin >> i;
-      }
-      //31 because max bit could be 30th bit in an positive int
-      vector<int> nSetBit(31); //number of elements with ith bit set
-      int count;
-      int term;
-      for(int bit =0; bit<=30;bit++){
-         
-          count = 0;
-          for(int& i : a){
-              if(i%2!=0){
-                  count ++;
-                 
-              }
-             
-              i/=2;
-          }
-         
-          nSetBit[bit] = count;
-      }
-     
-     
-      ans = 0;
-            for(int bit =0; bit<=30;bit++){
-                if(nSetBit[bit]%k == 0){
-                    ans += nSetBit[bit]/k;
-                }
-                else{
-                    ans += nSetBit[bit]/k + 1;
-                }
-     
-     
-  }
- 
-  cout << ans << endl;
+    int1 n,k,count,result=0;
+    cin >>n>>k;
+    vector<int> numbers(n);
+    vector<int> bits(31);
+    for(int i=0;i<n;i++)
+        cin>>numbers[i];
+    for(int i=0;i<31;i++)
+    {
+        count=0;
+        for(int j=0;j<n;j++)
+        {
+            if(numbers[j]%2!=0)
+                count++;  
+            numbers[j]/=2;
+        }
+        bits[i]=count;
+    }
+    for(int i=0;i<31;i++)
+    {
+        if (bits[i]%k==0)
+        result+= bits[i]/k;
+        else
+        result+= bits[i]/k + 1;
+    }
+    cout<<result<<endl;
+    
 }
-
-// for(auto x:array) cout<<x;
-// vector<int> num(5,0); ==> [0,0,0,0,0]
-// memset(data,-1,sizeof(data));
-// sort(array1,array1+n);
-
-//debug time
-// begin=clock(); end=clock(); cout<<(double)(end-begin)/CLOCKS_PER_SEC;
 int main()
 {
     ios_base::sync_with_stdio(0);
