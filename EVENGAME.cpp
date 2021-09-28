9278dab04adedf066af1f31e16722a7c2e4a048b
AvinashSubhash <avinashsubhash19@gmail.com>
2021-09-28 21:14:24 +0530

    added_two_programs

---
+++ EVENGAME.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+#define int1 long long int
+#define endl "\n"
+using namespace std;
+
+void solution()
+{
+    //Solving area
+    int1 n,sum1=0,t;
+    cin>>n;
+    for(int1 i=0;i<n;i++)
+    {
+        cin>>t;
+        sum1+=t;
+    }
+    if (sum1%2)cout<<2<<endl;
+    else
+    cout<<1<<endl;
+
+}
+
+// for(auto x:array) cout<<x;
+// vector<int> num(5,0); ==> [0,0,0,0,0]
+// memset(data,-1,sizeof(data));
+// sort(array1,array1+n);
+// map<int,int> data; data[t]++;
+//debug time
+// begin=clock(); end=clock(); cout<<(double)(end-begin)/CLOCKS_PER_SEC;
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    int1 t;
+    cin>>t;
+    while(t--)
+    {
+        solution();
+    }
+}
